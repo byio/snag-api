9eb5f5d335ab32fcb2a58d1c2dc65c76b36c54bf
byio <ben@benyang.io>
2018-07-25 07:52:57 +0800

    set up morgan logger, dotenv config, and connect to mlab

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 // start app
 const app = express();
+
+// dotenv
+require('dotenv').config();
+
+// connect to mlab
+const uri = `mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@ds137687.mlab.com:37687/snag`;
+mongoose.connect(uri, { useNewUrlParser: true })
+  .then(console.log(`Connected to mLab.`));
+
+// morgan logging middleware
+app.use(morgan('dev'));
 
 // general error handling
 app.use((req, res, next) => {
