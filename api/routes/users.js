1678d5a73b8190bd2deb57714325d857b7413c6c
byio <ben@benyang.io>
2018-07-25 20:16:12 +0800

    create users login function in users controller file, and import to use in users route file

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,5 +5,6 @@ const UserController = require('../controllers/users');
 const router = express.Router();
 
 router.post('/signup', UserController.users_signup);
+router.post('/login', UserController.users_login);
 
 module.exports = router;
