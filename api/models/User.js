884a3dda1a68249c10a9fa6e954f1a8192ac4b12
byio <ben@benyang.io>
2018-07-25 09:26:16 +0800

    create User model

---
+++ api/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const isSeller = (role) => role === 'seller';
+
+const userSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  role: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  countryCode: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: isSeller(role)
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
