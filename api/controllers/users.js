e2e2b7ad4a683a755433ffc71e299f19e2f26502
byio <ben@benyang.io>
2018-07-25 18:03:46 +0800

    fix code that causes users to be saved even when a 409 status is returned

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -15,38 +15,37 @@ exports.users_signup = (req, res, next) => {
   User.find({ email })
     .exec()
     .then(userArr => {
-      if (userArr.length > 0) {
-        if (!!(userArr.filter(user => user.accountType === accountType).length)) {
-          return res.status(409).json({
-            message: 'Email is already in use.'
-          });
-        }
-      }
-    })
-  // email checks out; hash password
-  bcrypt.hash(password, 10, (error, hash) => {
-    if (error) {
-      return res.status(500).json({ error });
-    } else {
-      const newUser = new User({
-        _id: mongoose.Types.ObjectId(),
-        accountType,
-        email,
-        password,
-        countryCode,
-        phoneNumber,
-        address
-      });
-      newUser.save()
-        .then(result => {
-          res.status(201).json({
-            message: 'Registration successful!',
-            result
-          });
-        })
-        .catch(error => {
-          res.status(500).json({ error });
+      if (userArr.filter(user => user.accountType === accountType).length > 0) {
+        return res.status(409).json({
+          message: 'Email is already in use.'
         });
-    }
-  });
+      } else {
+        bcrypt.hash(password, 10, (error, hash) => {
+          if (error) {
+            return res.status(500).json({ error });
+          } else {
+            const newUser = new User({
+              _id: mongoose.Types.ObjectId(),
+              accountType,
+              email,
+              password,
+              countryCode,
+              phoneNumber,
+              address
+            });
+            newUser.save()
+              .then(result => {
+                res.status(201).json({
+                  message: 'Registration successful!',
+                  result
+                });
+              })
+              .catch(error => {
+                res.status(500).json({ error });
+              });
+          }
+        });
+      }
+    });
+
 };
