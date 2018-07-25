e52c9782433ff530b527d668c3d6515f74629181
byio <ben@benyang.io>
2018-07-25 08:16:54 +0800

    set up middleware to handle CORS

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ mongoose.connect(uri, { useNewUrlParser: true })
 // morgan logging middleware
 app.use(morgan('dev'));
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // general error handling
 app.use((req, res, next) => {
   const error = new Error('Not Found');
